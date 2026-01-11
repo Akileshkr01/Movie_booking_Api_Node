dbe94dc743a094df4b3068bf27295fe94e249b7d
akil <akileshjm@gmail.com>
2026-01-11 23:21:04 +0530

    initial commit for Movie booking App

---
+++ index.js
@@ -0,0 +1,32 @@
+const env = require("dotenv");
+env.config();
+const express = require("express");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();//express app object
+
+//configure body parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get("/home", (req, res)=>{
+    return res.json({
+        success:true,
+        message:"fetched home"
+    });
+});
+
+app.listen(process.env.PORT, async() => {
+    // this callback gets executed .once we succesfully start the server on the given port 
+  console.log(`Server started on Port ${process.env.PORT}!!`);
+
+try {
+        // ENSURE YOUR .ENV FILE USES THE NAME "DB_URL"
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Successfully connected to mongo");
+    } catch (error) {
+        console.error("Mongoose connection error:", error);
+    }
+});
+
+   
