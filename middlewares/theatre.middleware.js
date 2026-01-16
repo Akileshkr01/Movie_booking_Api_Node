6e199bb85c86bc7ed5a52e0a605aca8bb066ba73
akil <akileshjm@gmail.com>
2026-01-16 16:41:27 +0530

    Feat:added the middleware and its routes to code

---
+++ middlewares/theatre.middleware.js
@@ -0,0 +1,23 @@
+const { errorResponseBody }= require('../utils/responseBody');
+const validateTheatreCreateRequest = async(req,res,next) => {
+    // validation for the presence of name
+    if(!req.body.name){
+        errorResponseBody.message =" the name of the theatre is not present in the request ";
+        return res.status(400).json(errorResponseBody);
+    }
+    //validation for the presence of pincode
+    if(!req.body.pincode){
+        errorResponseBody.message =" the pincode of the theatre is not present in the request";
+        return res.status(400).json(errorResponseBody);
+    }
+    //validation for the presence of city
+    if(!req.body.city){
+        errorResponseBody.message = "the city of the tehatre is not present in  the request";
+        return res.status(400).json(errorResponseBody);
+    }
+    next(); // everything is fine move to the next middleware
+}
+
+module.exports = {
+    validateTheatreCreateRequest
+}
