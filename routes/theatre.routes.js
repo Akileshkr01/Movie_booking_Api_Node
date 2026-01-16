dae6e470da7037e2215d8f1fa60edbb3852fea09
akil <akileshjm@gmail.com>
2026-01-16 16:41:27 +0530

    Feat:added the middleware and its routes to code

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,10 +1,12 @@
 const theatreController = require('../controllers/theatre.controller');
+const theatreMiddleware = require('../middlewares/theatre.middleware');
 
 const routes = (app) => {
     console.log('Theatre routes registered');
     
     app.post(
-        '/mba/api/v1/theatres',
+        '/mba/api/v1/theatres', 
+        theatreMiddleware.validateTheatreCreateRequest,
         theatreController.create
     );
 };
