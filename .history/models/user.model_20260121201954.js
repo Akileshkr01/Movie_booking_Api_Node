30e6bb39d87f32ec0e3b5ca04438c3a1d16cab77
akil <akileshjm@gmail.com>
2026-01-21 20:23:10 +0530

    Feat:Added user Schema

---
+++ .history/models/user.model_20260121201954.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match : [ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$,'Please fill a valid email'],
+        lowercase: true ,
+        
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength:10
+    },
+    userRole: {
+        type: String,
+        required: true,
+        default:"CUSTOMER"
+    },
+    userStatus: {
+        type: String,
+        required: true,
+        default: "Approved"
+    }
+},{ timestamps: true});
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
