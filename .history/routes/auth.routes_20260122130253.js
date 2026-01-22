be81e3c22548254e3f351533cffd26ba1610ad97
akil <akileshjm@gmail.com>
2026-01-22 14:08:22 +0530

    Feat: Added sign in logic

---
+++ .history/routes/auth.routes_20260122130253.js
@@ -0,0 +1,13 @@
+const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middlewares/auth.middlewares');
+const routes = (app) => {
+    app.post(
+        '/mba/api/v1/auth/signup',
+        authMiddleware.validateSignupRequest,
+        authController.signup
+        );
+
+    app.post
+};
+
+module.exports = routes;
