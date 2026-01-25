95929999ddf81262ac4a4dc6b5f5828192a464d8
akil <akileshjm@gmail.com>
2026-01-25 18:26:35 +0530

    FIX: Added constants for https codes in response

---
+++ .history/controllers/user.controller_20260125140207.js
@@ -0,0 +1,36 @@
+const userService = require("../services/user.service");
+const {errorResponseBody,successResponseBody} = require('../utils/responseBody');
+const{} = require('../utils/constants')
+const update = async (req, res) => {
+    try {
+        const response = await userService.updateUserRoleOrStatus(
+            req.body,
+            req.params.id
+        );
+
+        return res.status(STATUS.OK).json({
+            success: true,
+            message: "Successfully updated the user",
+            data: response
+        });
+
+    } catch (error) {
+        if (error.err) {
+            return res.status(error.code || 400).json({
+                success: false,
+                err: error.err,
+                data: {}
+            });
+        }
+
+        return res.status().json({
+            success: false,
+            err: "Internal Server Error",
+            data: {}
+        });
+    }
+};
+
+module.exports = {
+    update
+};
---
+++ .history/utils/constants_20260125135645.js
@@ -0,0 +1,17 @@
+const USER_STATUS = {
+    APPROVED: 'Approved',
+    PENDING: 'Pending',
+    BLOCKED: 'Blocked'
+};
+
+const USER_ROLE = {
+    CUSTOMER: 'CUSTOMER',
+    ADMIN: 'ADMIN',
+    CLIENT: 'CLIENT'
+};
+
+
+module.exports = {
+    USER_ROLE,
+    USER_STATUS
+};
