b0d600d0c56b85ee2174b56d4c12b858995ca1fb
akil <akileshjm@gmail.com>
2026-01-27 02:07:48 +0530

    Feat:Added show model

---
+++ .history/models/show.model_20260127015358.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const showSchema = new mongoose.Schema({
+    theatreId:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true
+    },
+    movieId: {
+        type:mongoose.S
+    }
+})
---
+++ .history/models/show.model_20260127015542.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const showSchema = new mongoose.Schema({
+    theatreId:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true
+    },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    timing: {
+        tpe:String,
+        required: true
+    },
+    noOfSeats: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number
+    }
+})
