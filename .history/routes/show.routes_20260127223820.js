9149b4b2c1affb4650434c1393e594cee63d33c2
akil <akileshjm@gmail.com>
2026-01-28 00:38:19 +0530

    Feat:Added show update and delete apis

---
+++ .history/routes/show.routes_20260127223820.js
@@ -0,0 +1,25 @@
+const showController = require('../controllers/show.controller');
+const authMiddlewares = require('../middlewares/auth.middlewares');
+const showMiddlewares = require('../middlewares/show.middlewares');
+const routes = (app) => {
+    app.post(
+        '/mba/api/v1/show',
+        authMiddlewares.isAuthenticated,
+        authMiddlewares.isAdminOrClient,
+        showMiddlewares.validateCreateShowRequest,
+        showController.create
+    );
+
+    app.get(
+        '/mba/api/v1/show',
+        showController.getShow
+    );
+
+    app.delete(
+        '/mba/api/v1/show',
+        authMiddlewares.is
+    )
+}
+
+module.exports = routes;
+ 
