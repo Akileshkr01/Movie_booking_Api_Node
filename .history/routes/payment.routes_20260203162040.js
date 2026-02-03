ef5e13e8a6e2c26110758d5233cfaf66abeb99d6
akil <akileshjm@gmail.com>
2026-02-03 18:04:31 +0530

    Feat:Added get all payments

---
+++ .history/routes/payment.routes_20260203162040.js
@@ -0,0 +1,32 @@
+const paymentController  = require('../controllers/payment.controller');
+const authMiddlewares  = require('../middlewares/auth.middlewares');
+const paymentMiddlewares = require('../middlewares/payment.middlewares');
+
+const routes = (app) => {
+    app.post(
+        '/mba/api/v1/payments',
+        authMiddlewares.isAuthenticated,
+        paymentMiddlewares.verifyPaymentCreateRequest,
+        paymentController.create
+    );
+
+
+       app.get(
+        '/mba/api/v1/payments/all',
+        authMiddlewares.isAuthenticated,
+        paymentController.getAllPayment
+    );
+    app.get(
+        '/mba/api/v1/payments/:id',
+        authMiddlewares.isAuthenticated,
+        paymentController.getPaymentDetailsById
+    );
+
+    app.get(
+        '/mba/api/v1/payments/all',
+        authMiddlewares.isAuthenticated,
+        paymentController.getAllPayment
+    );
+}
+
+module.exports = routes;
